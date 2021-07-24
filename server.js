a97b98f300c4d6d62d686372aa6d9096324243c6
Yair Valbuena <yair.valbuena@gmail.com>
2021-07-24 03:31:07 -0500

    install dependencies

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const mysql = require('mysql')
+const myconnection = require('express-myconnection')
+
+const dbConfig = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'ventasDB'
+}
 
 const app = express()
 app.set('port', process.env.PORT || 9000)
+app.use(myconnection(mysql, dbConfig, 'single'))
 
+//rutas
 app.get('/', (req, res) => {
     res.send('Prueba de API')
 })
 
+
+//listen
 app.listen(app.get('port'), () => {
     console.log('server listening', app.get('port'))
 })
