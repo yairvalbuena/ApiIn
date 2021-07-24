9a4c970092c0803ddcc77d94c49f6bc4d1223592
Yair Valbuena <yair.valbuena@gmail.com>
2021-07-24 03:05:11 -0500

    first try

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = requiere('express')
+const express = require('express')
 
 const app = express()
 app.set('port', process.env.PORT || 9000)
 
+app.get('/', (req, res) => {
+    res.send('Prueba de API')
+})
 
 app.listen(app.get('port'), () => {
     console.log('server listening', app.get('port'))
